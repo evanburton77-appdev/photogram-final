fe5501ebb13abaf50e565aa147d15d9ae22d3969
Evan Burton <evanburton77@gmail.com>
2023-04-20 20:28:37 +0000

    generated FollowRequest table and associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_20_202455) do
+ActiveRecord::Schema.define(version: 2023_04_20_202645) do
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "likes", force: :cascade do |t|
     t.integer "user_id"
